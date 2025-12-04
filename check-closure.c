65e5c7ac6feab5be87fbc79904eb1c319cfd5fa4
sofiaarantes <sofiaarantes@gmail.com>
2025-12-03 22:07:47 -0300

    Fechos

--- check-closure.c
+++ check-closure.c
@@ -9,7 +9,7 @@
     Como rodar, primeiramente entramos na pasta do projeto e rodamos 
     gcc -lm check-closure.c -o check-closure.bin
 
-    ./check-closure.bin entrada.txt saida.txt
+    ./check-closure.bin entrada.txt saida
 */
 
 #include <stdlib.h>
@@ -85,6 +85,8 @@ void ler_arquivo(FILE* fp, TipoRelacao* relacao){
     }
 }
 
+// Imprime a relação da matriz original normalmente no arquivo de saída, e imprime os 
+// arcos a serem adicionados para o fecho em vermelho
 void gerar_dot(TipoRelacao *relacao, char *arquivo_saida, int **matriz_fecho) {
     FILE *fp = fopen(arquivo_saida, "w");
     if (!fp) {
@@ -112,16 +114,27 @@ void gerar_dot(TipoRelacao *relacao, char *arquivo_saida, int **matriz_fecho) {
         }
     }
 
+    // Arestas (relações) do fecho
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matriz_fecho[i][j] == 1) {
+                fprintf(fp, "    %d -> %d [color=red];\n", i + 1, j + 1);
+            }
+        }
+    }
+
     fprintf(fp, "}\n");
     fclose(fp);
 
     printf("[SUCESSO] Arquivo DOT gerado: %s\n", arquivo_saida);
 }
 
+// ================= Reflexiva ===================
+
 // A relação R em um conjunto A é chamada de reflexiva se (x, x) ∈ R para
 // todo elemento x ∈ A.
 int reflexiva(TipoRelacao *relacao){
-    for (int i = 0; i < relacao->tam_conjunto-1; i++) {
+    for (int i = 0; i < relacao->tam_conjunto; i++) {
         if ((relacao->matriz[i][i]) == 0){
             return 0;
         }
@@ -129,11 +142,34 @@ int reflexiva(TipoRelacao *relacao){
     return 1;
 }
 
+// Recebe uma struct de TipoRelacao e retorna uma matriz com apenas os arcos necessários para a 
+// matriz original ser uma relação reflexiva
+int** fecho_reflexivo(TipoRelacao *relacao) {
+    // Aloca matriz do fecho
+    int **matriz_fecho = alocar_matriz(relacao->tam_conjunto);
+
+    // Inicializa tudo com 0
+    for (int i = 0; i < relacao->tam_conjunto; i++)
+        for (int j = 0; j < relacao->tam_conjunto; j++)
+            matriz_fecho[i][j] = 0;
+
+    // Preenche os arcos necessários para que ela se torne uma relação reflexiva
+    for (int i = 0; i < relacao->tam_conjunto; i++) {
+        if (relacao->matriz[i][i] == 0) {
+            matriz_fecho[i][i] = 1;
+        }
+    }
+
+    return matriz_fecho;
+}
+
+// ================= Simétrica ===================
+
 // Uma relação R em um conjunto A é chamada de simétrica se (y, x) ∈ R
 // sempre que (x, y) ∈ R.
 int simetrica(TipoRelacao *relacao){
-    for (int i = 0; i < relacao->tam_conjunto-1; i++) {
-        for (int j = 0; j < relacao->tam_conjunto-1; j++) {
+    for (int i = 0; i < relacao->tam_conjunto; i++) {
+        for (int j = 0; j < relacao->tam_conjunto; j++) {
             // Percorre todos, se tem ida, entra no if
             if ((relacao->matriz[i][j]) == 1){
                 // Se não tem volta, retorna zero
@@ -147,14 +183,43 @@ int simetrica(TipoRelacao *relacao){
     return 1;
 }
 
+// Recebe uma struct de TipoRelacao e retorna uma matriz com apenas os arcos necessários para a 
+// matriz original ser uma relação simetrica
+int** fecho_simetrico(TipoRelacao *relacao) {
+    // Aloca matriz do fecho
+    int **matriz_fecho = alocar_matriz(relacao->tam_conjunto);
+
+    // Inicializa tudo com 0
+    for (int i = 0; i < relacao->tam_conjunto; i++)
+        for (int j = 0; j < relacao->tam_conjunto; j++)
+            matriz_fecho[i][j] = 0;
+
+    // Preenche os arcos necessários para que ela se torne uma relação simétrica
+    for (int i = 0; i < relacao->tam_conjunto; i++) {
+        for (int j = 0; j < relacao->tam_conjunto; j++) {
+            // Percorre todos, se tem ida, entra no if
+            if ((relacao->matriz[i][j]) == 1){
+                // Se não tem volta, retorna zero
+                if ((relacao->matriz[j][i]) == 0){
+                    matriz_fecho[j][i] = 1;
+                }   
+            }
+        }
+    }
+    
+    return matriz_fecho;
+}
+
+// ================= Transitiva ===================
+
 // Uma relação R em um conjunto A é chamada transitiva se, sempre que
 // (x, y) ∈ R e (y, z) ∈ R, então (x, z) ∈ R, ∀x, y, z ∈ A.
 int transitiva(TipoRelacao *relacao) {
-    for (int i = 0; i < relacao->tam_conjunto-1; i++) {
-        for (int j = 0; j < relacao->tam_conjunto-1; j++) {
+    for (int i = 0; i < relacao->tam_conjunto; i++) {
+        for (int j = 0; j < relacao->tam_conjunto; j++) {
             // Percorre todos, se xRy, entra no if
             if ((relacao->matriz[i][j]) == 1){
-                for (int k = 0; k < relacao->tam_conjunto-1; k++) {
+                for (int k = 0; k < relacao->tam_conjunto; k++) {
                     // Se yRz, entra no if
                     if ((relacao->matriz[j][k]) == 1){
                         // Se x não se relaciona com z, retorna 0
@@ -170,6 +235,37 @@ int transitiva(TipoRelacao *relacao) {
     return 1;
 }
 
+// Recebe uma struct de TipoRelacao e retorna uma matriz com apenas os arcos necessários para a 
+// matriz original ser uma relação transitiva
+int** fecho_transitivo(TipoRelacao *relacao) {
+    // Aloca matriz do fecho
+    int **matriz_fecho = alocar_matriz(relacao->tam_conjunto);
+
+    // Inicializa tudo com 0
+    for (int i = 0; i < relacao->tam_conjunto; i++)
+        for (int j = 0; j < relacao->tam_conjunto; j++)
+            matriz_fecho[i][j] = 0;
+
+    // Preenche os arcos necessários para que ela se torne uma relação transitiva
+    for (int i = 0; i < relacao->tam_conjunto; i++) {
+        for (int j = 0; j < relacao->tam_conjunto; j++) {
+            // Percorre todos, se xRy, entra no if
+            if ((relacao->matriz[i][j]) == 1){
+                for (int k = 0; k < relacao->tam_conjunto; k++) {
+                    // Se yRz, entra no if
+                    if ((relacao->matriz[j][k]) == 1){
+                        // Se x não se relaciona com z, retorna 0
+                        if ((relacao->matriz[i][k]) == 0){
+                            matriz_fecho[i][k] = 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    return matriz_fecho;
+}
 
 int main(int argc, char **argv)
 {
@@ -191,10 +287,10 @@ int main(int argc, char **argv)
 
     TipoRelacao relacao;
 
+    // Lê o arquivo e preenche a struct(tam_conjunto e matriz binária)
     ler_arquivo(fp, &relacao);
 
-    int** matriz;
-    gerar_dot(&relacao, StrSaida, matriz);
+    // Preenche os dados das propriedades da relação recebida
 
     relacao.reflexiva = reflexiva(&relacao);
     printf("Reflexiva: %d\n", relacao.reflexiva);
@@ -204,6 +300,60 @@ int main(int argc, char **argv)
 
     relacao.transitiva = transitiva(&relacao);
     printf("Transitiva: %d\n", relacao.transitiva);
+
+    // Fecho Reflexivo
+
+    // matriz_fecho recebe a matriz preenchida na sub-rotina com os arcos necessários para a relação 
+    // ter a propriedade reflexiva
+    int** matriz_fecho_reflexivo = fecho_reflexivo(&relacao);
+    printf("\nMatriz Fecho Reflexivo: \n");
+    for (int i = 0; i < relacao.tam_conjunto; i++){
+        for (int j = 0; j < relacao.tam_conjunto; j++){
+            printf(" %d ", matriz_fecho_reflexivo[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Concatena nome recebido por comando para o nome do arquivo no fecho
+    char nome_saida[200];
+    snprintf(nome_saida, sizeof(nome_saida), "%s-ref.dot", StrSaida);
+    gerar_dot(&relacao, nome_saida, matriz_fecho_reflexivo);
+
+    // Fecho Simétrico
+
+    // matriz_fecho recebe a matriz preenchida na sub-rotina com os arcos necessários para a relação 
+    // ter a propriedade simétrica
+    int** matriz_fecho_simetrico = fecho_simetrico(&relacao);
+    printf("\nMatriz Fecho Simetrico: \n");
+    for (int i = 0; i < relacao.tam_conjunto; i++){
+        for (int j = 0; j < relacao.tam_conjunto; j++){
+            printf(" %d ", matriz_fecho_simetrico[i][j]);
+        }
+        printf("\n");
+    }
+    // Concatena nome recebido por comando para o nome do arquivo no fecho
+    char nome_saida[200];
+    snprintf(nome_saida, sizeof(nome_saida), "%s-sim.dot", StrSaida);
+    gerar_dot(&relacao, nome_saida, matriz_fecho_simetrico);
+
+    // Fecho Transitivo
+
+    // matriz_fecho recebe a matriz preenchida na sub-rotina com os arcos necessários para a relação 
+    // ter a propriedade transitiva
+    int** matriz_fecho_transitivo = fecho_transitivo(&relacao);
+    printf("\nMatriz Fecho Transitivo: \n");
+    for (int i = 0; i < relacao.tam_conjunto; i++){
+        for (int j = 0; j < relacao.tam_conjunto; j++){
+            printf(" %d ", matriz_fecho_transitivo[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Concatena nome recebido por comando para o nome do arquivo no fecho
+    char nome_saida[200];
+    snprintf(nome_saida, sizeof(nome_saida), "%s-tra.dot", StrSaida);
+    gerar_dot(&relacao, nome_saida, matriz_fecho_transitivo);
+
 
     /* Encerra a aplicacao */
     exit(0);
